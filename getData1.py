0d93536a1d3da04b2b77f02b46e91e4e44499d74
Bikram Kashyap <kashyap.bikram@gmail.com>
2015-03-03 22:14:02 -0500

    Initial Commit

---
+++ getData1.py
@@ -0,0 +1,78 @@
+from os import listdir
+import os, sys
+from os.path import isfile, join
+
+
+path = "/home/bikramka/Downloads/andresultsTXTfiles";
+dirs = os.listdir(path);
+diction_h ={'dummy':{}};
+diction_c ={'dummy':{}};
+cursiveCount = 0;
+hadprintCounnt = 0;
+#valuesListCursivne = [];
+#valuesListHandprint = [];
+#indexCursiveBadRecord = [];
+#indexHandprintBadRecord = [];
+for year in dirs:
+	hand_type =  os.listdir(path+"/"+year);
+	for h_type in hand_type:
+		grade = os.listdir(path+"/"+year+"/"+h_type);
+		for g in grade:
+			files =  os.listdir(path+"/"+year+"/"+h_type+"/"+g);
+			for f in files:
+				with open(path+"/"+year+"/"+h_type+"/"+g+"/"+f) as input:
+   					 for line in input:
+						line = line[:line.rfind(',')];
+						val = line.split(',');
+						if(len(val) >12):
+							name = val[0].split(' ')[0];
+							name = name.replace("11-12","").replace("12-13","").replace("13-14","").replace("-","");
+							t = val[0][val[0].rfind(' '):].strip();
+							if(t!="cursive" and t!="c" and t!="printing" and t!="p"):
+								if(h_type=="cursive"):
+									t = "cursive";
+								else:
+									t = "printing";
+								
+#							v = t+" "+g.replace(" ","")+" "+" ".join(val[-12:]);
+                                                        v = "".join(val[-12:]);
+                                                        v = v.strip();
+                                                        #v=[x.strip() for x in v];
+                                                        #v=[int(x) for x in v]
+							if(t!="cursive" and t!="c"):
+								#v = "printing"+" "+g.replace(" ","")+" "+" ".join(val[-12:]);
+								if(diction_h.has_key(g)):
+									l = diction_h.get(g);
+									l.append(v);
+									diction_h.update({g:l})
+								else:
+									l=[v];
+									diction_h.update({g: l})
+							elif(t=="cursive" or t=="c"):	
+								#v = "cursive"+" "+g.replace(" ","")+" "+" ".join(val[-12:]);
+								if(diction_c.has_key(g)):
+									l = diction_c.get(g);
+									l.append(v);
+									diction_c.update({g:l})
+								else:
+									l=[v];
+									diction_c.update({g: l})
+print("cursive");
+for x in diction_c:
+    #print (x)
+    #for y in diction_c[x]:
+     #   print (y)
+    if len(diction_c[x]) > 1:
+        print(x+","+str(len(diction_c[x])));
+        #print(y)
+        
+print("handwritten");        
+for x in diction_h:
+    #print (x)
+    #for y in diction_c[x]:
+     #   print (y)
+    if len(diction_h[x]) > 1:
+        #print(x)
+        print(x+","+str(len(diction_h[x])));
+        
+        #print(y)    
